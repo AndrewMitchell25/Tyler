4a4102b621333c80628fdd6c0d534aa84177161e
AndrewMitchell25 <andrew.j.mitchell.247@gmail.com>
2022-04-11 12:59:16 -0400

    callback working

--- functions/index.js
+++ functions/index.js
@@ -4,21 +4,24 @@ admin.initializeApp();
 
 const dbRef = admin.firestore().collection('tokens');
 
-//const keys = await dbRef.doc('keys').get();
-//const { clientID, clientSecret } = keys.data();
-const TwitterApi = require('twitter-api-v2').default;
-const twitterClient = new TwitterApi({
-        clientId: '',
-        clientSecret: '',
-    });
+async function createTC(){
+    const keys = await dbRef.doc('keys').get();
+    const { clientID, clientSecret } = keys.data();
 
-
-
+    const TwitterApi = require('twitter-api-v2').default;
+    const twitterClient = new TwitterApi({
+        clientId: clientID,
+        clientSecret: clientSecret,
+        });
+    return twitterClient;
+}
 
 
 const callbackURL = 'http://127.0.0.1:5000/tyler-76c59/us-central1/callback';
 
 exports.auth = functions.https.onRequest(async (request, response) => {
+    const twitterClient = await createTC();
+
     const { url, codeVerifier, state } = twitterClient.generateOAuth2AuthLink(
         callbackURL,
         {scope: ['tweet.read', 'tweet.write', 'users.read', 'offline.access']}
@@ -33,23 +36,33 @@ exports.auth = functions.https.onRequest(async (request, response) => {
 });
 
 exports.callback = functions.https.onRequest(async (request, response) => {
+    const twitterClient = await createTC();
 
     const { state, code } = request.query;
 
     const dbSnapshot = await dbRef.doc('data').get();
     const { codeVerifier, state: storedState } = dbSnapshot.data();
-    console.log(dbSnapshot.data());
 
     if (state != storedState) {
         return response.status(400).send('Stored tokens do not match!');
     }
 
-    const {} = await twitterClient.loginWithOAuth2({
+    const {
+        client: loggedClient,
+        accessToken,
+        refreshToken,
+    } = await twitterClient.loginWithOAuth2({
         code,
         codeVerifier,
-        //6:11
+        redirectUri: callbackURL,
     });
+
+    await dbRef.doc('access').set({ accessToken, refreshToken });
+
+    response.sendStatus(200);
 
 });
 
-exports.tweet = functions.https.onRequest((request, response) => {});
+exports.tweet = functions.https.onRequest((request, response) => {
+    
+});
